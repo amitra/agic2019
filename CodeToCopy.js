0b352c16b151b22028bd937aa5e6b65e2e02c61c
ayan_m <mitra.ayan@gmail.com>
2019-10-01 15:17:00 -0700

    add code files for agic 2019 wab workshop

---
+++ CodeToCopy.js
@@ -0,0 +1,15 @@
+﻿//In widget.html , after line 28
+
+<div class="AGICCustomCount" style="color:red;font-weight: 700;"></div>
+
+
+
+//In item-list.js, line 1113
+
+        // add between Line 1113 and line 1114, after the closing brace on 1113 and before  return maxResultCount; 
+
+        //start - add in my custom count
+        console.log(maxResultCount);
+        var divPropCount = query(".AGICCustomCount")[0];
+        domAttr.set(divPropCount, "innerHTML", "Total Number of Features Found : " + maxResultCount);   
+        //end - add in my custom count
